90b15c0cf9ae717c2349c9bf0d447075ef19bac6
uwe72 <61848539+uwe72@users.noreply.github.com>
2021-01-16 13:37:52 +0100

    Add files via upload

---
+++ startCameraDetection.sh
@@ -0,0 +1,6 @@
+export CLASSPATH=./lib/mail.jar:./lib/log4j-1.2.9.jar:./lib/detection.jar:./lib/opencv-450.jar:./res:./lib/activation.jar:./lib/telegrambots-2.4.4.5-jar-with-dependencies.jar:./lib/activation.jar:./lib/commons-codec-1.11.jar:./lib/commons-io-2.6.jar:./lib/commons-logging-1.2.jar:./lib/httpasyncclient-4.1.4.jar:./lib/httpclient-4.5.11.jar:./lib/httpcore-4.4.13.jar:./lib/httpcore-nio-4.4.10.jar:./lib/istack-commons-runtime-3.0.11.jar:./lib/jakarta.activation-1.2.2.jar:./lib/jakarta.xml.bind-api-2.3.3.jar:./lib/jaxb-runtime-2.3.3.jar:./lib/nextcloud-api-11.3.0.jar:./lib/sardine-5.9.jar:./lib/slf4j-api-1.7.30.jar:./lib/txw2-2.3.3.jar
+export iobroker_ip=192.168.178.210:8087
+java de.clement.detection.Detection config_linux.properties
+
+
+
